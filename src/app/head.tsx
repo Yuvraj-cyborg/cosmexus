01c63bb64be1f1751bfa6346745f895f7e4c68e0
Yuvraj <yuvrajbiswalofficial@gmail.com>
2025-10-19 11:23:19 +0530

    fix: favicon-good-quality

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/space-shuttle.png" type="image/png" sizes="any" />
+      <link rel="apple-touch-icon" href="/space-shuttle.png" />
+    </>
+  );
+}
+
+
